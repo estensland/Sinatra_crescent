8b7236ae75e058167ea6bae58dc1c6952fbcf69d
Eric Stensland <estensland88@gmail.com>
2014-03-06 16:19:58 -0600

    working on person associations

--- app/models/classical_muslim.rb
+++ app/models/classical_muslim.rb
@@ -1,5 +1,18 @@
 class ClassicalMuslim < ActiveRecord::Base
-  # has_many
-  # belongs_to
+  has_many :adherents
+  has_many :factions, through: :adherents
+  has_many :ethnic_identities
+  has_many :ethnicities, through: :ethnic_identities
+  has_many :positions
+  has_many :titles, through: :positions  
+  has_many :scopes, through: :positions
+  has_many :life_events
+  has_many :events, through: :life_events
+  
+  has_many :subject_relationships, class_name: "relationships"
+  has_many :object_relationships, class_name: "relationships"
+  
+
+
   # Remember to create a migration!
 end
